bf039d22302dfa8a84142bb314ddd7660ace09fd
luceliaandrade <luceliara@hotmail.com>
2024-06-27 17:14:59 -0300

    alteração

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+    title: 'Lucelia Ribeiro de Andrade',
+    date: new Date().toLocaleDateString()
+    time: new Date().toLocaleTimeString()
+  });
 });
 
 module.exports = router;
