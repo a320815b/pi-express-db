01e790e28e6402ec71a009ef4aa8deda96958c40
luceliaandrade <luceliara@hotmail.com>
2024-07-03 17:20:11 -0300

    ..]

--- routes/alunos.js
+++ routes/alunos.js
@@ -6,25 +6,19 @@ var alunos = require("../tests/mocks/alunos.json");
 router.get("/", function (req, res, next) {
     const data = {
         title: "Alunos",
-        alunos: alunos
+        alunos: alunos,
     };
 
     res.render("list", data);
+});
+router.get("/new", function (req, res, next) {
+    res.render("form", { title: "Novo Aluno", buttonText: "Adicionar" });
 });
 
 router.get("/:matricula", function (req, res, next) {
     const { matricula } = req.params;
     const aluno = alunos.content[matricula];
- res.render("card", { title: "Detalhe do Aluno", aluno });
-});
-
-
-
-router.get("/new", function (req, res, next) {
-    const { matricula } = req.params;
-    const aluno = alunos.content[matricula];
-
-    res.render("card", { title: "Novo Aluno", buttontext:'Adicinar' });
+    res.render("card", { title: "Detalhe do Aluno", aluno });
 });
 
 module.exports = router;
